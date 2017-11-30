ea968d402102677e82a5a632c8e7e2e0120f9d95
valekar <srinivas.valekar@gmail.com>
2017-11-30 17:54:15 +0530

    adde

--- frontend/src/app/pages/models/URLS.ts
+++ frontend/src/app/pages/models/URLS.ts
@@ -1,7 +1,12 @@
 export class URLS{
-    public static RANDOM_SENTENCE_URL = "/api/random/sentence/";
-    public static GET_NLTK_SCORE_URL = "/api/opensource/score/";
-    public static GET_AZURE_SCORE_URL = "/api/azure/score/";
-    public static GET_WATSON_SCORE_URL = "/api/watson/score/";
-    public static GET_GOOGLE_SCORE_URL = "/api/google/score/";
+    //prod
+    public static cloud_provider = "https://my-cloud-project.herokuapp.com";
+    //dev
+    //public static cloud_provider = "";
+    
+    public static RANDOM_SENTENCE_URL = URLS.cloud_provider + "/api/random/sentence/";
+    public static GET_NLTK_SCORE_URL = URLS.cloud_provider +"/api/opensource/score/";
+    public static GET_AZURE_SCORE_URL = URLS.cloud_provider +"/api/azure/score/";
+    public static GET_WATSON_SCORE_URL = URLS.cloud_provider + "/api/watson/score/";
+    public static GET_GOOGLE_SCORE_URL = URLS.cloud_provider + "/api/google/score/";
 }
